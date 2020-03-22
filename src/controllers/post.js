47ee69d8d882467e3b326ce1ffbfca8cb2869896
paulloclara <paulloclara@gmail.com>
2020-03-22 14:40:51 -0300

    :rocket: Config post and index routes of Post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,9 +1,30 @@
 const Post = require("../models/post");
 
 module.exports = {
-  async index(request, response) {},
+  async index(request, response) {
+    const posts = await Post.find().sort("-createAt");
+
+    return response.status(200).json(posts);
+  },
+
   async show(request, response) {},
-  async store(request, response) {},
+
+  async store(request, response) {
+    const { filename: image } = request.file;
+    const { author, place, description, hashtag } = request.body;
+
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtag,
+      image
+    });
+
+    return response.status(200).json(post);
+  },
+
   async update(request, response) {},
+
   async destroy(request, response) {}
 };
