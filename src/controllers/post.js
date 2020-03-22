d2d4107d0eb5b99512bb4227a4a6340e18dfeafa
paulloclara <paulloclara@gmail.com>
2020-03-22 13:06:10 -0300

    :rocket: Config initial post routes

---
+++ src/controllers/post.js
@@ -0,0 +1,9 @@
+const Post = require("../models/post");
+
+module.exports = {
+  async index(request, response) {},
+  async show(request, response) {},
+  async store(request, response) {},
+  async update(request, response) {},
+  async destroy(request, response) {}
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const PostController = require("./controllers/post");
 
-router.get("/", (request, response) => {
-  response.send({ message: "hello world" });
-});
+router.get("/posts", PostController.index);
+router.get("/posts/:id", PostController.show);
+router.post("/posts", PostController.store);
+router.put("/posts", PostController.update);
+router.delete("/posts", PostController.destroy);
 
 module.exports = {
   config(server) {
