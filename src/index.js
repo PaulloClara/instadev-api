d39ddc78c683f5b8346d1f7706a7bf3d70c66c35
paulloclara <paulloclara@gmail.com>
2020-03-22 16:13:27 -0300

    :rocket: Config cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require("./services/mongo").config();
 
 const server = require("express")();
 
+require("./config/cors").config(server);
 require("./config/static-path").config(server, ["..", "uploads"]);
+
 require("./routes").config(server);
 
 const port = 3000;
