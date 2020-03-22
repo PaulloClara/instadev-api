be149e502593b07752054ae8436897013fe13d2c
paulloclara <paulloclara@gmail.com>
2020-03-22 17:25:50 -0300

    :bug: File name in database is not being updated

--- src/middleware/resize.js
+++ src/middleware/resize.js
@@ -3,12 +3,13 @@ const { resolve: resolvePath } = require("path");
 const { unlinkSync: removeFile } = require("fs");
 
 module.exports = async (request, response, next) => {
+  request.file.filename = `${request.file.filename.split(".")[0]}.jpg`;
   const { path, destination, filename: name } = request.file;
 
   await sharp(path)
     .resize(500)
     .jpeg({ quality: 70 })
-    .toFile(resolvePath(destination, `${name.split(".")[0]}.jpg`));
+    .toFile(resolvePath(destination, name));
 
   removeFile(path);
 
