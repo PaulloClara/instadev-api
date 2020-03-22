6db9261f95722fff61bca8633b969359c90035b7
paulloclara <paulloclara@gmail.com>
2020-03-22 15:43:52 -0300

    :rocket: Config resize image middleware

---
+++ src/middleware/resize.js
@@ -0,0 +1,16 @@
+const sharp = require("sharp");
+const { resolve: resolvePath } = require("path");
+const { unlinkSync: removeFile } = require("fs");
+
+module.exports = async (request, response, next) => {
+  const { path, destination, filename: name } = request.file;
+
+  await sharp(path)
+    .resize(500)
+    .jpeg({ quality: 70 })
+    .toFile(resolvePath(destination, `-${name}`));
+
+  removeFile(path);
+
+  return next();
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 
 const PostController = require("./controllers/post");
+
 const UploadMiddleware = require("./middleware/upload");
+const ResizeMiddleware = require("./middleware/resize");
 
 router.get("/posts", PostController.index);
 router.get("/posts/:id", PostController.show);
-router.post("/posts", UploadMiddleware.single("image"), PostController.store);
+router.post(
+  "/posts",
+  UploadMiddleware.single("image"),
+  ResizeMiddleware,
+  PostController.store
+);
 router.put("/posts", PostController.update);
 router.put("/posts/:id/likes", PostController.updateLike);
 router.delete("/posts", PostController.destroy);
