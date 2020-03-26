a17fd08dffd440192febf1cf3a9e9b18319e8b5b
paulloclara <paulloclara@gmail.com>
2020-03-25 21:38:59 -0300

    :rocket: Load new image

--- src/middleware/resize-img.js
+++ src/middleware/resize-img.js
@@ -1,17 +1,22 @@
 const sharp = require("sharp");
 const { resolve: resolvePath } = require("path");
-const { unlinkSync: removeFile } = require("fs");
+const { unlinkSync: removeFile, createReadStream: readFile } = require("fs");
 
 module.exports = async (request, response, next) => {
-  request.file.filename = `${request.file.filename.split(".")[0]}.jpg`;
-  const { path, destination, filename: name } = request.file;
+  const { path, destination } = request.file;
+  const name = `${request.file.filename.split(".")[0]}.jpg`;
 
-  await sharp(path)
+  const { size } = await sharp(path)
     .resize(500)
     .jpeg({ quality: 70 })
     .toFile(resolvePath(destination, name));
 
   removeFile(path);
+  request.file.size = size;
+  request.file.filename = name;
+  request.file.path = `${request.file.path.split(".")[0]}.jpg`;
+
+  request.file.buffer = readFile(resolvePath(destination, name));
 
   return next();
 };
